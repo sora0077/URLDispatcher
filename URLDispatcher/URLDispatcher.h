bce78376104646025fdf9eaf613d129c028ddf59
t.hayashi <t.hayashi0077+bitbucket@gmail.com>
2014-12-03 01:50:17 +0900

    Initial Commit

---
+++ URLDispatcher/URLDispatcher.h
@@ -0,0 +1,19 @@
+//
+//  URLDispatcher.h
+//  URLDispatcher
+//
+//  Created by 林 達也 on 2014/12/03.
+//  Copyright (c) 2014年 林 達也. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for URLDispatcher.
+FOUNDATION_EXPORT double URLDispatcherVersionNumber;
+
+//! Project version string for URLDispatcher.
+FOUNDATION_EXPORT const unsigned char URLDispatcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <URLDispatcher/PublicHeader.h>
+
+
